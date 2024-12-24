176f290b6fb57d652d76c444178237c0be3bd8ff
Okpara202 <okparafavour202@gmail.com>
2024-12-24 09:02:45 +0100

    fixed landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import Quiz from "./components/quiz";
 import Title from "./components/title";
+import Instruction from "./components/instructions";
 
 function App() {
 
   const router = createBrowserRouter(createRoutesFromElements(
     <>
       <Route path="/" element={<Title />} >
+        <Route index element={<Instruction />} />
         <Route path="quiz" element={<Quiz />} />
       </Route>
     </>
   ))
-  
+
   return (
     <div className="container">
 
